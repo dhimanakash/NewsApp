e0c9d10e507c24422c712b17cd29c5976e54c96d
akash dhiman <officialdhiman8@gmail.com>
2021-09-23 22:07:54 +0530

    Homepage styling complete

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,7 @@ const HomePage = () => {
   const [apiData, setApiData] = useState([]);
   const getData = async () => {
     const response = await axios.get(
-      "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=9ab2ae99801f4590842cbdd9c6b508c2"
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=9ab2ae99801f4590842cbdd9c6b508c2"
     );
     setApiData(response.data.articles);
     console.log(response.data.articles, "data");
@@ -19,25 +19,39 @@ const HomePage = () => {
 
   return (
     <>
-      <h1 style={{ margin: "10px 0px" }}>Top Headlines 💯</h1>
-      {apiData.map((val) => {
-        return (
-          <div className="card" key={val.publishedAt}>
-            <img src={val.urlToImage} alt="Avatar" style={{ width: "100%" }} />
-            <div className="container">
-              <h4>
-                <b>{val.title}</b>
-              </h4>
-              <p>{val.description}</p>
+      <h1 style={{ margin: "10px 0px", textAlign: "center" }}>
+        Top Headlines 💯
+      </h1>
+      <div className="outer-container">
+        {apiData.map((val) => {
+          const { publishedAt, urlToImage, title, description, url } = val;
+          return (
+            <div className="card" key={publishedAt}>
+              <img
+                src={
+                  urlToImage
+                    ? urlToImage
+                    : "https://static.toiimg.com/thumb/msid-86453061,width-1070,height-580,imgsize-86304,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg"
+                }
+                alt="Avatar"
+                style={{ width: "100%" }}
+              />
+
+              <div className="container">
+                <h4 style={{ margin: "10px 0px" }}>
+                  <b>{title}</b>
+                </h4>
+                <p>{description}</p>
+              </div>
+              <button>
+                <a href={url} target="_blank" rel="noreferrer">
+                  Read more
+                </a>
+              </button>
             </div>
-            <button>
-              <a href={val.url} target="_blank">
-                Read more
-              </a>
-            </button>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </>
   );
 };
